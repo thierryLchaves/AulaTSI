8f3b4d7ab17420470638879db0319199adfe7a3f
275322 <275322@35180293-LAB205.utramigbh.sede>
2019-06-27 21:41:31 -0300

    o cassio é top

--- src/cassioaula/CassioAula.java
+++ src/cassioaula/CassioAula.java
@@ -16,6 +16,9 @@ public class CassioAula {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+       System.out.print("Olá mundo");
     }
     
 }
